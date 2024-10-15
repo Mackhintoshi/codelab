62ab4f4d51e3cf80899829a94173b6d006a81844
Jerick Gutierrez <45832168+Mackhintoshi@users.noreply.github.com>
2024-10-15 14:34:24 -0700

    Update page.tsx

--- jscodelab/app/page.tsx
+++ jscodelab/app/page.tsx
@@ -11,10 +11,10 @@ const projects = [
     icon: "/webrtc-icon.png",
   },
   {
-    title: "AI Interview Chatbot",
-    description: "(took this app down) An AI Interview Coach, that helps you practice your interview skill. The app is built in NextJS and uses a Flask with Redis. It integrates with ChatGPT to provide a chatbot experience.",
-    url: "#",
-    icon: "/botIcon250.png",
+    title: "Event Planning App",
+    description: "An AI assistant event planner with one stop shop marketplace for events. This uses Dart for front end, and uses Python for backend, plus some PHP.",
+    url: "https://play.google.com/store/apps/details?id=com.the_black_pink_app&pli=1",
+    icon: "/tierez.png",
   },
   {
     title: "Python Flask Protobuf Extension",
@@ -23,17 +23,18 @@ const projects = [
     icon: "/flask-icon.png",
   },
   {
+    title: "AI Interview Chatbot",
+    description: "(took the backend down for now) An AI Interview Coach, that helps you practice your interview skill. The app is built in NextJS and uses a Flask with Redis. It integrates with ChatGPT to provide a chatbot experience.",
+    url: "#",
+    icon: "/botIcon250.png",
+  },
+  
+  {
     title: "Girl Band News App",
     description: "(took this app down) This app allows you to access latest news about Blackpink from different sources, Choose your bias, Track how we are achieving our goals as Blinks. i.e How many views do we need to achieve? ,A simple plain screen that can be used on concerts if you do not have blink lights.",
     url: "https://play.google.com/store/apps/details?id=com.the_black_pink_app&pli=1",
     icon: "/blinkApp.png",
   },
-  {
-    title: "Event Planning App",
-    description: "An AI assistant event planner with one stop shop marketplace for events. This uses Dart for front end, and uses Python for backend, plus some PHP.",
-    url: "https://play.google.com/store/apps/details?id=com.the_black_pink_app&pli=1",
-    icon: "/tierez.png",
-  }
 ]
 
 export default function IndexPage() {
